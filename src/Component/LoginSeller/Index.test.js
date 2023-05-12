9d05767b0e316fad8741f330a663db7050722aa9
arnabnath180 <arnabnath3030@gmail.com>
2023-05-12 20:33:26 +0530

    Update test

--- src/Component/LoginSeller/Index.test.js
+++ src/Component/LoginSeller/Index.test.js
@@ -1,23 +1,24 @@
 import Index from './Index'
 import {render, screen} from '@testing-library/react'
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { store } from '../../State/Store.js';
 
 describe("Test Index",()=>{
     test("render with 1 buttons",async()=>{
-        render(<Router><Index/></Router>)
+        render(<Provider store={store}><Router><Index/></Router></Provider>)
         const btnList=await screen.findAllByRole("button");
         expect(btnList).toHaveLength(1);
     })
 
     test("check password type",()=>{
-        render(<Router><Index/></Router>)
+        render(<Provider store={store}><Router><Index/></Router></Provider>)
         const password=screen.getByPlaceholderText("Password");
         expect(password).toHaveAttribute("type","password");
     })
 
     test("check email type",()=>{
-        render(<Router><Index/></Router>)
+        render(<Provider store={store}><Router><Index/></Router></Provider>)
         const email=screen.getByPlaceholderText("Enter email");
         expect(email).toHaveAttribute("type","email");
     })
--- src/Component/SellerHome/Index.test.js
+++ src/Component/SellerHome/Index.test.js
@@ -3,21 +3,24 @@ import RenderedComponent1 from '../SellerDashboard/Index.js';
 import RenderedComponent2 from '../SellerHistory/Index.js';
 import {fireEvent, render, screen} from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import { store } from '../../State/Store.js';
 
 describe("Test Index",()=>{
     test("render with 3 buttons",async()=>{
-        render(<Router><Index/></Router>)
+        render(<Provider store={store}><Router><Index/></Router></Provider>)
         const btnList=await screen.findAllByRole("button");
         expect(btnList).toHaveLength(3);
     })
 
     test("sell button click", async()=>{
         const { queryByTestId }=render(
+            <Provider store={store}>
             <Router>
                 <Index/>
                 <RenderedComponent1/>
             </Router>
+            </Provider>
         )
         const sell=screen.getByRole("button",{name:"Sell Scrap"})
         fireEvent.click(sell)
@@ -26,10 +29,12 @@ describe("Test Index",()=>{
 
     test("history button click", ()=>{
         const { queryByTestId }=render(
+            <Provider store={store}>
             <Router>
                 <Index/>
                 <RenderedComponent2/>
             </Router>
+            </Provider>
         )
         const history=screen.getByRole("button",{name:"Scrap History"})
         fireEvent.click(history)
